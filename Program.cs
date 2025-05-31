20979e4bb462b829959161f967c8e3e371fda822
Miguel Dias <mdias1300@gmail.com>
2025-05-30 21:20:22 -0300

    atividade feita

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ Thread.Sleep(2000);
 Console.ResetColor();
 
 
-Console.WriteLine($"salario-hora {5.5:C}, {720} horas, sendo {40} horas-extra, salário = {Salario(5.5, 720, 40):C}");
+Console.WriteLine($"salario-hora {5.5:C}, {720} horas, sendo {4.0} horas-extra, salário = {Salario(5.5, 720, 4.0):C}");
 
 double Salario(double salarioHora, double horasTotais, double horasExtras)
 {
-    return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 40);
+    return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 4.0);
 }
