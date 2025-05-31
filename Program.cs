df1c088655b5202d53d0cdde584926cfc7ec40ae
Miguel Dias <mdias1300@gmail.com>
2025-05-30 21:17:34 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("..... Horas Extras.....");
+Thread.Sleep(2000);
+Console.ResetColor();
+
+
+Console.WriteLine($"salario-hora {5.5:C}, {720} horas, sendo {40} horas-extra, salário = {Salario(5.5, 720, 40):C}");
+
+double Salario(double salarioHora, double horasTotais, double horasExtras)
+{
+    return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 40);
+}
